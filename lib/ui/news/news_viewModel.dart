372eff90e5e33c624252baf61d57df595fe9135c
Nagham Abdelrahman <naghamabdelrahman62@gmail.com>
2023-02-25 09:23:21 -1200

    updated to mvvm pattern using bloc

--- lib/ui/news/news_viewModel.dart
+++ lib/ui/news/news_viewModel.dart
@@ -1,25 +1,50 @@
-import 'package:news/base/base_viewModel.dart';
-import 'package:news/core/api/api_manager.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../core/api/api_manager.dart';
 import '../../core/model/News.dart';
-import 'news_navigator.dart';
 
-class NewsListViewModel extends BaseViewModel<NewsNavigator> {
-  String? errorMessage;
-  List<News>? newsList;
+class NewsListViewModel extends Cubit<NewsListState> {
+  NewsListViewModel() : super(LoadingState());
 
   void getNewsBySourceId(String sourceId) async {
-    errorMessage = null;
-    notifyListeners();
     try {
       var response = await ApiManager.getNews(soureID: sourceId);
       if (response.status == 'error') {
-        errorMessage = 'Server Error \n${response.message}';
+        emit(ErrorState('Server Error \n${response.message}'));
+      } else {
+        emit(NewsLoadedState(response.articles!));
       }
-      newsList = response.articles!;
     } catch (e) {
-      errorMessage = 'error loading news';
+      emit(ErrorState('Error loading News'));
     }
-    notifyListeners();
   }
+
+  void getNewsBySearch(String word) async {
+    try {
+      var response = await ApiManager.getNews(searchWord: word);
+      if (response.status == 'error') {
+        emit(ErrorState('Server Error \n${response.message}'));
+      } else {
+        emit(NewsLoadedState(response.articles!));
+      }
+    } catch (e) {
+      emit(ErrorState('Error loading News'));
+    }
+  }
+}
+
+abstract class NewsListState {}
+
+class LoadingState extends NewsListState {}
+
+class ErrorState extends NewsListState {
+  String errorMessage;
+
+  ErrorState(this.errorMessage);
+}
+
+class NewsLoadedState extends NewsListState {
+  List<News> newsList;
+
+  NewsLoadedState(this.newsList);
 }
