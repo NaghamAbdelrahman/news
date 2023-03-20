979a1096264a2c4dd43ff59866fcc88be0786d12
Nagham Abdelrahman <naghamabdelrahman62@gmail.com>
2023-03-20 05:59:13 -1200

    added dependency injection in app layer

--- lib/presentation/category/di.dart
+++
@@ -1,6 +0,0 @@
-import 'package:news/data/repository/sources_repository_impl.dart';
-import 'package:news/domain/usecases/getSourcesByCategoryIdUseCase.dart';
-
-GetSourcesByCategoryIdUseCase injectGetSourcesByCategoryIdUseCase() {
-  return GetSourcesByCategoryIdUseCase(injectSourcesRepository());
-}
